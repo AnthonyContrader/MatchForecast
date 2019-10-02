90d1c609d8c29db0f60ca566364e0086ac681dc5
Alessandro Garofalo <alessandro.g4rofalo@gmail.com>
2019-10-02 12:13:34 +0200

    PartitaConverter

---
+++ src/main/java/it/contrader/converter/PartitaConverter.java
@@ -0,0 +1,41 @@
+
+
+package it.contrader.converter;
+
+import java.util.ArrayList;
+import java.util.List;
+import it.contrader.dto.PartitaDTO;
+import it.contrader.model.Partita;
+
+
+
+
+
+	
+	public class PartitaConverter   {
+		
+		
+		public PartitaDTO toDTO(Partita partita) {
+			PartitaDTO partitaDTO = new PartitaDTO(partita.getId(), partita.getSquadra1(), partita.getSquadra2(), partita.getData() , partita.getOrario() ,partita.getRisultato());
+			return partitaDTO;
+		}
+
+		
+		public Partita toEntity(PartitaDTO partitaDTO) {
+			User user = new Partita(partitaDTO.getId, partitaDTO.getSquadra1(), partitaDTO.getSquadra2(), partitaDTO.getData() , partitaDTO.getOrario() ,partitaDTO.getRisultato());
+			return partita;
+		}
+		
+		
+		public List<PartitaDTO> toDTOList(List<Partita> partitaList) {
+			
+			List<PartitaDTO> partitaDTOList = new ArrayList<PartitaDTO>();
+			
+			
+			for(Partita partita : partitaList) {
+				
+				partitaDTOList.add(toDTO(partita));
+			}
+			return partitaDTOList;
+		} 
+	
