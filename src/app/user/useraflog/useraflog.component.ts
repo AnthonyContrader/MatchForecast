d55628f70e8a1f5f1d971a0743ca47cbc2e4b1ba
giuseppe15889 <55691296+giuseppe15889@users.noreply.github.com>
2019-11-12 09:30:04 +0100

    fix investimento

--- src/app/user/useraflog/useraflog.component.ts
+++ src/app/user/useraflog/useraflog.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { InvestimentoDTO } from 'src/dto/investimentodto';
-import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
 import { InvestimentoService } from 'src/service/investimento.service';
-import { SchedinaService } from 'src/service/schedina.service';
+import { InvestimentoDTO } from 'src/dto/investimentodto';
+import {UserService} from 'src/service/user.service';
 import { UserDTO } from 'src/dto/userdto';
+
 import { SchedinaDTO } from 'src/dto/schedinadto';
-import { UserService } from 'src/service/user.service';
 
 @Component({
   selector: 'app-useraflog',
@@ -15,54 +13,57 @@ import { UserService } from 'src/service/user.service';
 })
 export class UseraflogComponent implements OnInit {
 
+ 
+
+  
   investimento: InvestimentoDTO;
   investimentos: InvestimentoDTO[];
   investimentotoinsert: InvestimentoDTO = new InvestimentoDTO();
   users: UserDTO [];
   user: UserDTO;
-  schedinas: SchedinaDTO [];
-  schedina: SchedinaDTO;
 
-  constructor(private service: InvestimentoService, private router: Router, private t2Service: SchedinaService, 
-    private tService: UserService) { }
-
-  
+  constructor(private service: InvestimentoService,private tService: UserService) { }
 
   ngOnInit() {
-
-    this.user = JSON.parse(localStorage.getItem('currentUser'));
-    this.schedina = JSON.parse(localStorage.getItem('currentSchedina'));
-
+    this.user = JSON.parse(localStorage.getItem('AUTOKEN'));
+    console.log(this.user);
+    console.log(localStorage);
     this.getInvestimentos();
     this.getUsers();
-    this.getSchedinas();
+    
     
   }
 
   getInvestimentos() {
-    
+    if (this.user.id == this.investimento.iduser) {
     this.service.getAll().subscribe(investimentos => this.investimentos = investimentos);
+    }
   }
 
   getUsers() {
+    
     this.tService.getAll().subscribe(users => this.users = users);
     
   }
 
-  getSchedinas() {
+ 
+  delete(investimento: InvestimentoDTO) {
+    this.service.delete(investimento.id).subscribe(() => this.getInvestimentos());
+  }
+
+  update(investimento: InvestimentoDTO) {
    
-    this.t2Service.getAll().subscribe(schedinas => this.schedinas = schedinas);
-  }
-  
-  
-  nv(investimento: InvestimentoDTO) : void {
-
-    this.router.navigate(['/user-dashboard']);
-    //investimento.user = this.user;
-    
-    console.log(investimento);
-    this.service.insert(investimento).subscribe(() => this.getInvestimentos());       
-
+    this.service.update(investimento).subscribe(() => this.getInvestimentos());
   }
 
+  insert(investimento: InvestimentoDTO) {
+    investimento.iduser = this.user.id;
+  console.log(investimento);
+  console.log(this.user);
+    this.service.insert(investimento).subscribe(() => this.getInvestimentos());
+  }
+
+  clear(){
+    this.investimentotoinsert = new InvestimentoDTO();
+  }
 }
